8f47e4d1306157143d6737239d2ed83679be5b2f
MalavikaAjit <malavikaajit9@gmail.com>
2021-07-30 01:54:46 +0530

    ADD: added day 1 and day 2 learning

--- Learning_day2/script.js
+++ Learning_day2/script.js
@@ -1,1 +1,16 @@
 alert("do it")
+//filter
+const students = [
+    { name: 'Quincy', grade: 96 },
+    { name: 'Jason', grade: 84 },
+    { name: 'Alexis', grade: 100 },
+    { name: 'Sam', grade: 65 },
+    { name: 'Katie', grade: 90 }
+  ];
+  
+  const studentGrades = students.filter(student => student.grade >= 90);
+  return studentGrades;
+
+  
+
+    
